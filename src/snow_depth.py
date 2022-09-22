db582585ef1c4538a31ce15b6b04d81efa26f097
modengann <99417989+modengann@users.noreply.github.com>
2022-09-22 09:55:41 -0400

    init

---
+++ src/snow_depth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def snow_depth():
+    pass
+
+def main():
+    result = snow_depth()
+    print(f"Max snow depth: {result}")
+
+if __name__ == "__main__":
+    main()
